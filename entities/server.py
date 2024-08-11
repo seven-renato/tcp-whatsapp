4a0947a41a6ef9112fb0c433f66251e6534786d5
seven-renato <seven.renato@hotmail.com>
2024-08-11 16:12:33 -0300

    adding new codes

--- entities/server.py
+++ entities/server.py
@@ -13,12 +13,34 @@ class Server:
         self.codes = {
             '01': self.register_client,
             '03': self.connect_client,
+            '05': self.send_message,
+            '08': self.confirm_read
         }
+
+    def deliver_message(self, conn, message):
+        send_message = '06' + message[2:]
+        deliver_message = '07' + message[2:]
+        conn.sendall(send_message.encode())
+        src = message[2:16]
+        if src in self.connected_clients:
+            conn = self.connected_clients[src]
+            conn.sendall(deliver_message.encode())
+        else:
+            self.unreceived_messages[src].append(message)
+    
+    def confirm_read(self, payload, conn):
+        src = payload[:14]
+        timestamp = payload[14:25]
+        if src in self.connected_clients:
+            conn = self.connected_clients[src]
+            dst = list(self.connected_clients.keys())[list(self.connected_clients.values()).index(conn)]
+            conn.sendall(f'09{dst}{timestamp}'.encode())
 
     def register_client(self, payload, conn):
         id = str(len(self.created_clients))
         self.created_clients += 1
         res = '02' + id
+        self.unreceived_messages[id] = []
         
         conn.sendall(res.encode())
 
@@ -27,8 +49,17 @@ class Server:
 
         unreceived_messages = self.unreceived_messages[payload]
         for message in unreceived_messages:
-            conn.sendall(message.encode())
-        #enviar mensagens pendentes se tiver
+            self.deliver_message(conn, message)
+        self.unreceived_messages[payload] = []
+
+    def send_message(self, payload, conn):
+        dst = payload[14:28]
+        server_payload = payload
+        if dst in self.connected_clients:
+            conn = self.connected_clients[dst]
+            self.deliver_message(conn, server_payload)
+        else:
+            self.unreceived_messages[dst].append(server_payload)
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
